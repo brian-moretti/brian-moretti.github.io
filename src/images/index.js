b48cc46c4a2b429c46b4920248b8bfc6ad2797ad
brian-moretti <brianmoretti2512@gmail.com>
2023-02-22 16:04:41 +0100

    Added html and images

--- src/images/index.js
+++ src/images/index.js
@@ -6,15 +6,19 @@ export const jquery = require("./jquery.svg");
 export const wordpress = require("./wordpress.svg");
 export const git = require("./git.svg");*/
 
-import html from './skills/html.svg';
-import css from './skills/css.svg';
-import js from './skills/js.svg';
-import sass from './skills/sass.svg';
-import jquery from './skills/jquery.svg';
-import git from './skills/git.svg';
-import wordpress from './skills/wordpress.svg';
-import vite from './skills/vite.svg';
+import mockup from "./mockup-portfolio/mockup-cogesca.png";
 
-export {html, css, js, sass, jquery, git, wordpress, vite}
+//* SKILL IMAGE
+
+import html from "./skills/html.svg";
+import css from "./skills/css.svg";
+import js from "./skills/js.svg";
+import sass from "./skills/sass.svg";
+import jquery from "./skills/jquery.svg";
+import git from "./skills/git.svg";
+import wordpress from "./skills/wordpress.svg";
+import vite from "./skills/vite.svg";
+
+export { html, css, js, sass, jquery, git, wordpress, vite };
 
 //? È possibile creare una funzione per automatizzare l'import / export delle immagini nella cartella
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,40 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        portfolio: resolve(__dirname, 'portfolio/index.html'),
-        progettiStar2impact: resolve(__dirname, 'progetti-star2impact/index.html'),
+        main: resolve(__dirname, "index.html"),
+        portfolio: resolve(__dirname, "portfolio/index.html"),
+        progettiStar2impact: resolve(
+          __dirname,
+          "progetti-star2impact/index.html"
+        ),
+        studiosetecoservice: resolve(
+          __dirname,
+          "portfolio/studio-seteco-service/index.html"
+        ),
+        ambulatorioaura: resolve(
+          __dirname,
+          "portfolio/ambulatorio-aura/index.html"
+        ),
+        cogescaAppalti: resolve(
+          __dirname,
+          "portfolio/cogesca-appalti/index.html"
+        ),
+        mtp: resolve(
+          __dirname,
+          "portfolio/mtp-ambulatorio-odontoiatrico/index.html"
+        ),
+        odontoiatriaProgresso: resolve(
+          __dirname,
+          "portfolio/odontoiatria-progresso/index.html"
+        ),
+        studioTassi: resolve(__dirname, "portfolio/studio-tassi/index.html"),
       },
     },
   },
-})
+  assetsInclude: ["**/*.gltf"],
+});
